c1bb5c487d93c748b71ad04c00837e6b0efa7fa5
Alexandr Efimchik <efimchiksasha@gmail.com>
2022-10-10 16:49:40 +0300

    refactor: code style applied

--- Labs/LAB_02/String/String.h
+++ Labs/LAB_02/String/String.h
@@ -11,20 +11,21 @@
 class String {
  public:
   String();
-//  String(const String& arg_string);
+  String(const String& arg_string);
   explicit String(char *arg_string);
 
-  String& operator=(char *arg_string);
-  String& operator=(const String &arg_string);
+  String &operator=(char *arg_string);
+  String &operator=(const String &arg_string);
   char operator[](int arg_index);
-  char* operator()(int arg_left_index, int arg_right_index);
-  String operator+(const String &arg_string);
-//  String& operator+=(const String &arg_string);
+  char *operator()(int arg_left_index, int arg_right_index);
+  String &operator+(const String &arg_string);
+  String &operator+=(const String &arg_string);
   bool operator==(const String &arg_string);
   bool operator!=(const String &arg_string);
 
   [[nodiscard]] char *GetString() const;
   [[nodiscard]] int GetLength() const;
+  void Append(const String &arg_string);
 
   ~String();
 
